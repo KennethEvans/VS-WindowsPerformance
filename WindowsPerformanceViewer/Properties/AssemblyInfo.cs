9ba2584b1c3c5a926f4533e68e2736ed0ca4d7d7
Kenneth Evans Jr <ken@kenevans.net>
2011-07-29 01:25:26 +0000

    Added AboutBox.  Had to make several changes in the template for it to work right.
    git-svn-id: http://svn.kenevans.net/VisualStudio/trunk@132 4b487630-4e8c-464e-b16a-428ddfb157b8

--- WindowsPerformanceViewer/Properties/AssemblyInfo.cs
+++ WindowsPerformanceViewer/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsPerformanceViewer")]
+[assembly: AssemblyTitle("Windows Performance Viewer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("WindowsPerformanceViewer")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("kenevans.net")]
+[assembly: AssemblyProduct("Windows Performance Viewer")]
+[assembly: AssemblyCopyright("Copyright © Kenneth Evans 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
